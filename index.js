729d714a70d09260393e6a7ad723d770cd475da4
Kei Son <heyacct@gmail.com>
2015-04-28 20:53:24 +0900

    add to support `Promise`

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var xmlrpc = require('xmlrpc');
 var util = require('util');
+var bluebird = require('bluebird');
 var config = require('./config');
 
 function Long(raw) {
@@ -16,23 +17,52 @@ exports.connect = function connect(host, port, path) {
     client = xmlrpc.createClient({ host: host, port: port, path: path });
 };
 
+function makeResolve(callback, resolve) {
+    if (!callback) return resolve;
+    return function () {
+        var args = Array.prototype.slice.call(arguments);
+        return callback.apply(null, [null].concat(args));
+    };
+}
+
+function makeReject(callback, reject) {
+    return callback || reject;
+}
+
 exports.login = function login(id, password, callback) {
-    if (!client) return callback(new Error('Not Connected'));
-    client.methodCall('confluence2.login', [id, password], function (err, newToken) {
-        if (err) return callback(err);
-        token = newToken;
-        return callback();
+    return new bluebird(function (res, rej) {
+        res = makeResolve(callback, res);
+        rej = makeReject(callback, rej);
+        if (!client) return rej(new Error('Not Connected'));
+        client.methodCall('confluence2.login', [id, password], function (err, newToken) {
+            if (err) return rej(err);
+            token = newToken;
+            return res();
+        });
     });
 };
 
 exports.getPage = function getPage(pageId, callback) {
-    if (!client) return callback(new Error('Not Connected'));
-    if (!token) return callback(new Error('Not Logged-in'));
-    client.methodCall('confluence2.getPage', [token, new Long(pageId)], callback);
+    return new bluebird(function (res, rej) {
+        res = makeResolve(callback, res);
+        rej = makeReject(callback, rej);
+        if (!client) return rej(new Error('Not Connected'));
+        if (!token) return rej(new Error('Not Logged-in'));
+        client.methodCall('confluence2.getPage', [token, new Long(pageId)], function (err, page) {
+            return res(page);
+        });
+    });
 };
 
 exports.getPages = function getPages(spaceKey, callback) {
-    if (!client) return callback(new Error('Not Connected'));
-    if (!token) return callback(new Error('Not Logged-in'));
-    client.methodCall('confluence2.getPages', [token, spaceKey], callback);
+    return new bluebird(function (res, rej) {
+        res = makeResolve(callback, res);
+        rej = makeReject(callback, rej);
+        if (!client) return rej(new Error('Not Connected'));
+        if (!token) return rej(new Error('Not Logged-in'));
+        client.methodCall('confluence2.getPages', [token, spaceKey], function (err, pages) {
+            if (err) return rej(err);
+            return res(pages);
+        });
+    });
 };

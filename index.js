3fc3f0d6b85d653f9f43ddc10b384dc77aaa2df8
heycalmdown <heyacct@gmail.com>
2015-12-25 16:32:34 +0900

    refactor) Upgrade to REST API from xmlrpc

--- index.js
+++ index.js
@@ -1,121 +1,85 @@
-var xmlrpc = require('xmlrpc');
-var util = require('util');
-var bluebird = require('bluebird');
-var config = require('./config');
+var Promise = require('bluebird');
+var superagent = require('superagent-bluebird-promise');
 
-function Long(raw) {
-  xmlrpc.CustomType.call(this, raw);
-};
-
-util.inherits(Long, xmlrpc.CustomType);
-
-Long.prototype.tagName = 'Long';
+function GET(uri) {
+  var prefix = '/rest/api';
+  if (uri.slice(0, prefix.length) === prefix) {
+    prefix = '';
+  }
+  return client.get(host + prefix + uri).then(function (data) {
+    return data.body;
+  });
+}
 
 var client;
 var token;
-exports.connect = function connect(host, port, path) {
-    client = xmlrpc.createClient({ host: host, port: port, path: path });
+var host;
+exports.connect = function connect(host_) {
+  client = superagent.agent();
+  host = host_;
 };
 
-function makeResolve(callback, resolve) {
-    if (!callback) return resolve;
-    return function () {
-        var args = Array.prototype.slice.call(arguments);
-        return callback.apply(null, [null].concat(args));
-    };
+function checkCondition() {
+  if (!client) throw new Error('Not Connected');
+  // if (!token) return rej(new Error('Not Logged-in'));
 }
 
-function makeReject(callback, reject) {
-    return callback || reject;
-}
-
-exports.login = function login(id, password, callback) {
-    return new bluebird(function (res, rej) {
-        res = makeResolve(callback, res);
-        rej = makeReject(callback, rej);
-        if (!client) return rej(new Error('Not Connected'));
-        client.methodCall('confluence2.login', [id, password], function (err, newToken) {
-            if (err) return rej(err);
-            token = newToken;
-            return res();
-        });
-    });
-};
-
-exports.getPage = function getPage(pageId, callback) {
-    return new bluebird(function (res, rej) {
-        res = makeResolve(callback, res);
-        rej = makeReject(callback, rej);
-        if (!client) return rej(new Error('Not Connected'));
-        if (!token) return rej(new Error('Not Logged-in'));
-        client.methodCall('confluence2.getPage', [token, new Long(pageId)], function (err, page) {
-            return res(page);
-        });
-    });
-};
-
-exports.getChildren = function getChildren(pageId, callback) {
-  return new bluebird(function (res ,rej) {
-    res = makeResolve(callback, res);
-    rej = makeReject(callback, rej);
-    if (!client) return rej(new Error('Not Connected'));
-    if (!token) return rej(new Error('Not Logged-in'));
-    client.methodCall('confluence2.getChildren', [token, new Long(pageId)], function (err, summaries) {
-      return res(summaries);
-    });
+exports.getPage = function getPage(pageId) {
+  return Promise.resolve().then(function () {
+    checkCondition();
+    return GET('/content/' + pageId);
   });
 };
 
-exports.getPages = function getPages(spaceKey, callback) {
-    return new bluebird(function (res, rej) {
-        res = makeResolve(callback, res);
-        rej = makeReject(callback, rej);
-        if (!client) return rej(new Error('Not Connected'));
-        if (!token) return rej(new Error('Not Logged-in'));
-        client.methodCall('confluence2.getPages', [token, spaceKey], function (err, pages) {
-            if (err) return rej(err);
-            return res(pages);
-        });
-    });
+exports.getChildren = function getChildren(pageId) {
+  return Promise.resolve().then(function () {
+    checkCondition();
+    return GET('/content/' + pageId + '/child/page');
+  });
 };
 
-exports.getSpaces = function getSpaces(callback) {
-    return new bluebird(function (res, rej) {
-        res = makeResolve(callback, res);
-        rej = makeReject(callback, rej);
-        if (!client) return rej(new Error('Not Connected'));
-        if (!token) return rej(new Error('Not Logged-in'));
-        client.methodCall('confluence2.getSpaces', [token], function (err, spaces) {
-            if (err) return rej(err);
-            return res(spaces);
-        });
-    });
+function _getPagesAll(query, pages) {
+  pages = pages || [];
+  return GET(query).then(function (body) {
+    pages = pages.concat(body.results);
+    if (!body._links.next) return pages;
+    return _getPagesAll(body._links.next, pages);
+  });
+}
+
+exports.getPages = function getPages(spaceKey, opts) {
+  opts = opts || {};
+  opts.limit = opts.limit || 25;
+  return Promise.resolve().then(function () {
+    checkCondition();
+    var query = '/space/' + spaceKey + '/content/page';
+    if (!opts.all) return GET(query).then(function (body) { return body.results; });
+    return _getPagesAll(query + '?limit=' + opts.limit);
+  });
 };
 
-exports.getSpace = function getSpace(spaceKey, callback) {
-    return new bluebird(function (res, rej) {
-        res = makeResolve(callback, res);
-        rej = makeReject(callback, rej);
-        if (!client) return rej(new Error('Not Connected'));
-        if (!token) return rej(new Error('Not Logged-in'));
-        client.methodCall('confluence2.getSpace', [token, spaceKey], function (err, space) {
-            if (err) return rej(err);
-            return res(space);
-        });
-    });
+function _getSpacesAll(query, spaces) {
+  spaces = spaces || [];
+  return GET(query).then(function (body) {
+    spaces = spaces.concat(body.results);
+    if (!body._links.next) return spaces;
+    return _getSpacesAll(body._links.next, spaces);
+  });
+}
+
+exports.getSpaces = function getSpaces(opts) {
+  opts = opts || {};
+  opts.limit = opts.limit || 25;
+  return Promise.resolve().then(function () {
+    checkCondition();
+    if (!opts.all) return GET('/space').then(function (body) { return body.results; });
+    return _getSpacesAll('/space?limit=' + opts.limit);
+  });
 };
 
-exports.getSpaceStatus = function getSpaceStatus(spaceKey, callback) {
-    return new bluebird(function (res, rej) {
-        res = makeResolve(callback, res);
-        rej = makeReject(callback, rej);
-        if (!client) return rej(new Error('Not Connected'));
-        if (!token) return rej(new Error('Not Logged-in'));
-        client.methodCall('confluence2.getSpaceStatus', [token, spaceKey], function (err, spaceStatus) {
-            if (err) return rej(err);
-            return res(spaceStatus);
-        });
-    });
+exports.getSpace = function getSpace(spaceKey) {
+  return Promise.resolve().then(function () {
+    checkCondition();
+    return GET('/space/' + spaceKey);
+  });
 };
-
-// vim: set sts=4 sw=4 ts=8 et:
--- test/index.js
+++ test/index.js
@@ -1,9 +1,47 @@
 var confluency = require('..');
+require('should');
 
 // you need to set the config.js correctly to run this test.
 describe('default', function () {
-  it('should get the page', function (done) {
-    confluency.connect('https://confluence.atlassian.com', 80);
-    confluency.getPage()
-  })
-})
+  before(function () {
+    confluency.connect('https://confluence.atlassian.com');
+  });
+  it('should get a page', function (done) {
+    confluency.getPage(139456).then(function (data) {
+    }).then(done, done);
+  });
+  it('should get child pages', function (done) {
+    confluency.getChildren(251005338).then(function (data) {
+    }).then(done, done);
+  });
+  it('should get spaces', function (done) {
+    confluency.getSpaces().then(function (spaces) {
+      spaces.should.be.an.Array();
+    }).then(done, done);
+  });
+  it('should get every spaces', function (done) {
+    this.timeout(10000);
+    confluency.getSpaces({all: true, limit: 500}).then(function (spaces) {
+      spaces.should.be.an.Array();
+      spaces.length.should.greaterThan(380);
+    }).then(done, done);
+  });
+  it('should get a space', function (done) {
+    confluency.getSpace('JIRA').then(function (space) {
+      space.should.have.property('key');
+      space.key.should.be.exactly('JIRA');
+    }).then(done, done);
+  });
+  it('should get pages in a space', function (done) {
+    confluency.getPages('JIRA').then(function (pages) {
+      pages.should.be.an.Array();
+    }).then(done, done);
+  });
+  it('should get every pages in a space', function (done) {
+    this.timeout(10000);
+    confluency.getPages('JIRA', {all: true, limit: 500}).then(function (pages) {
+      pages.should.be.an.Array();
+      pages.length.should.greaterThan(1200);
+    }).then(done, done);
+  });
+});
